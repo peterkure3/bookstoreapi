97b0255ddecb05140a03714bf6ba1da9d329581b
Peter Kure <peterkure256@gmail.com>
2024-04-18 06:34:01 +0300

    🚧Request

---
+++ src/main/java/com/example/bookstore/dto/requests/BookRequest.java
@@ -0,0 +1,32 @@
+package com.example.bookstore.dto.requests;
+
+public class BookRequest {
+
+    private String title;
+    private String isbn;
+    private double price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
