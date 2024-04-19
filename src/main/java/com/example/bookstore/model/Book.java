8bd02727de27fff686a6ccc7b697e977cae7d12f
Peter Kure <peterkure256@gmail.com>
2024-04-19 12:23:58 +0300

    modified:   src/main/java/com/example/bookstore/model/Book.java

--- src/main/java/com/example/bookstore/model/Book.java
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.example.bookstore.model;
 
 import java.sql.Date;
@@ -55,78 +52,59 @@ public class Book {
 
     @ManyToMany(mappedBy = "books")
     private Set<Category> categories;
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getIsbn() {
 		return isbn;
 	}
-
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
-
 	public double getPrice() {
 		return price;
 	}
-
 	public void setPrice(double price) {
 		this.price = price;
 	}
-
 	public Date getPublishedDate() {
 		return publishedDate;
 	}
-
 	public void setPublishedDate(Date publishedDate) {
 		this.publishedDate = publishedDate;
 	}
-
 	public String getCoverImage() {
 		return coverImage;
 	}
-
 	public void setCoverImage(String coverImage) {
 		this.coverImage = coverImage;
 	}
-
 	public Author getAuthor() {
 		return author;
 	}
-
 	public void setAuthor(Author author) {
 		this.author = author;
 	}
-
 	public Publisher getPublisher() {
 		return publisher;
 	}
-
 	public void setPublisher(Publisher publisher) {
 		this.publisher = publisher;
 	}
-
 	public Set<Category> getCategories() {
 		return categories;
 	}
-
 	public void setCategories(Set<Category> categories) {
 		this.categories = categories;
 	}
 
-    // Getters and Setters
 }
